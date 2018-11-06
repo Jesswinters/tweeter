df0c5028a405740001ebaec76ae815c54a828571
Jessica Winters <jskawinters@gmail.com>
2018-11-06 23:25:18 +0000

    Added tweet HTML and CSS, refactored character counter JS

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,19 +1,19 @@
 // Update character counter on keyup
-// Variables with $ denote jQuery object
 
 $(document).ready(function() {
-  $('#new-tweet-form').on('keyup', () => {
+  $('#new-tweet-form').on('input', (event) => {
+    const target = $(event.currentTarget);
+    const currentLength = target.find('.new-tweet-textarea').val().length;
+    const characterCounter = target.find('.counter');
     const maxLength = 140;
-    const $currentLength = $(this).find('.new-tweet-textarea').val().length;
-    const $characterCounter = $(this).find('.counter');
-    let newCount = maxLength - $currentLength;
+    let newCount = maxLength - currentLength;
 
-    $characterCounter.html(newCount);
+    characterCounter.html(newCount);
 
-    if ($currentLength > maxLength) {
-      $characterCounter.addClass('error');
+    if (currentLength > maxLength) {
+      characterCounter.addClass('error');
     } else {
-      $characterCounter.removeClass('error');
+      characterCounter.removeClass('error');
     }
   });
 });
