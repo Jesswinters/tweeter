88e888f38492142986ecea94bfa1e3794dfbc6b8
Jessica Winters <jskawinters@gmail.com>
2018-11-08 00:13:35 +0000

    Loading new tweets with AJAX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,54 +5,6 @@
  */
 
 $(document).ready(function() {
-  // Fake data taken from tweets.json
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": {
-          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": {
-          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-      },
-      "content": {
-        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit. <b>test</b>"
-      },
-      "created_at": 1461113796368
-    }
-  ];
-
   // Escape HTML from Tweet content/text
   function escapeContent(string) {		
     const escapedText = document.createElement('span');
@@ -61,14 +13,16 @@ $(document).ready(function() {
 		return escapedText.innerHTML;
   }
 
+  // Loop through tweet data to pass to create function
   function renderTweets(data) {
     data.forEach((tweetData) => {
-      $('#tweets-container').append(createTweetElement(tweetData));
+      $('#tweets-container').prepend(createTweetElement(tweetData));
     });
   }
 
+  // Create tweet element from data using template literals
   function createTweetElement(tweetData) {
-    const tweetText = escapeContent(tweetData.content.text);	
+    const tweetText = escapeContent(tweetData.content.text);
 
     return `<article class="tweet">
         <header>
@@ -86,5 +40,26 @@ $(document).ready(function() {
       </article>`;
   }
 
-  renderTweets(data);
+  function fetchTweets() {
+    $.getJSON('/tweets')
+      .done((data) => {
+        renderTweets(data);
+      });
+  }
+
+  $('#new-tweet-button').on('click submit', (event) => {
+    event.preventDefault();
+    const $form = $(this).find('#new-tweet-form');
+
+    $.ajax({
+      type: 'POST',
+      url:  '/tweets',
+      data: $form.serialize(),
+    })
+    .done(() => {
+      fetchTweets();
+    });
+  });
+
+  fetchTweets();
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,19 +2,47 @@
 // Occurs when the text content of an element is changed through the user interface
 
 $(document).ready(function() {
-  $('#new-tweet-form').on('input', (event) => {
+  $('#new-tweet-form').on('input keyup', (event) => {
     const target = $(event.currentTarget);
-    const currentLength = target.find('.new-tweet-textarea').val().length;
+    const tweetTextarea = target.find('.new-tweet-textarea').val();
+    const submitButton = target.find('#new-tweet-button');
     const characterCounter = target.find('.counter');
-    const maxLength = 140;
-    let newCount = maxLength - currentLength;
+    const errorMessage = target.find('.error-message');
 
+    let maxLength = 140;
+    let newCount = maxLength - tweetTextarea.length;
     characterCounter.html(newCount);
 
-    if (currentLength > maxLength) {
+    // If current count of characters is longer than max length,
+    // display error message and change counter to red
+    if (tweetTextarea.length > maxLength) {
+      errorMessage
+        .html('Tweet content is over 140 characters.')
+        .addClass('visible');
       characterCounter.addClass('error');
     } else {
+      errorMessage
+        .html('')
+        .removeClass('visible');
       characterCounter.removeClass('error');
+    }
+
+    // If current count of characters is longer than max length or empty,
+    // disable button and display error message
+    if (tweetTextarea.length > maxLength || tweetTextarea.trim() === '') {
+      submitButton
+        .prop('disabled', true)
+        .addClass('disabled');
+      errorMessage
+        .html('Please enter a message.')
+        .addClass('visible');
+    } else {
+      submitButton
+        .prop('disabled', false)
+        .removeClass('disabled');
+      errorMessage
+        .html('')
+        .removeClass('visible');
     }
   });
 });
